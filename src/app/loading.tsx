2a9561c3e1f9fde86f0cce34c8d79203c80c2221
Jawdat <jawdat.89@gmail.com>
2023-10-31 21:35:16 +0200

    add loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <Image
+        width={150}
+        height={150}
+        src="spinner.svg"
+        alt="Loading..."
+        className="h-100"
+      />
+    </div>
+  );
+}
